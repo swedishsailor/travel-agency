4a49dcc959b79fd53657b441dcb05632dddaf1cd
swedishsailor <kzygowski7@gmail.com>
2021-08-15 21:07:09 +0200

    test data base

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,6 +4,43 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import { calculateTotal } from '../../../utils/calculateTotal';
+import { formatPrice } from '../../../utils/formatPrice';
+import settings from '../../../data/settings.js';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  if (options.name == '' || options.contact == '') {
+    alert('Provide correct data');
+  } else 
+  {
+
+    const fetchOptions = {
+      cache: 'no-cache',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    };
+
+    fetch(url, fetchOptions)
+      .then(function(response){
+        return response.json();
+      }).then(function(parsedResponse){
+        console.log('parsedResponse', parsedResponse);
+      });
+  }
+};
 
 const OrderForm = (props) => (
   <Grid>
@@ -15,6 +52,7 @@ const OrderForm = (props) => (
       ))}
       <Col xs={12}>
         <OrderSummary cost={props.tripCost} options={props.options}/>
+        <Button onClick={() => sendOrder(props.options, props.tripCost, props.name, props.id, props.country)}>Order now!</Button>
       </Col>
     </Row>
   </Grid>
@@ -24,6 +62,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.node,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  country: PropTypes.node,
 };
 
 export default OrderForm;
